832df80303e6e2f3e8d9b048407be00a624a0d3a
Luci-Codeing <3967742419@qq.com>
2026-02-20 13:45:15 +0800

    赤石代码-年龄计算器.cpp
    帮你知道你的年龄 [doge]

---
+++ 年龄计算器.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string Command = '"请输入你的年龄";
+    int UserAge;
+    cout << Command;
+    cin >> UserAge;
+    cout << "你的年龄为: " << UserAge << endl;
+}
